728c7ed59290e58ed0f97525e72288c7ce666a59
Rijalul Fikri <fikri.desertlion@gmail.com>
2020-07-11 15:06:06 +0700

    Initial Setup

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::inertia('/', 'auth/login')->name('index');
+Route::inertia('home', 'home')->name('home')->middleware('auth');
+
+Auth::routes();
